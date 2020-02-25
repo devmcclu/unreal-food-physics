f0fccda58fb6f2e789c4128d58eb4e886337ec55
Devlin McClure <mccluredt@gmail.com>
2020-02-24 22:25:50 -0500

    Try to add box collision in code, not working so far

--- Source/FoodPhysics/BaseFood.cpp
+++ Source/FoodPhysics/BaseFood.cpp
@@ -8,13 +8,16 @@ ABaseFood::ABaseFood()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	// CollisionMesh = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	// CollisionMesh->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
 void ABaseFood::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//CollisionMesh->OnComponentBeginOverlap.AddDynamic(this, &ABaseFood::OnOverlapBegin);
 }
 
 // Called every frame
@@ -26,7 +29,8 @@ void ABaseFood::Tick(float DeltaTime)
 
 void ABaseFood::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-
+	FRotator objectRotation = FRotator();
+	SpawnObject(GetActorLocation(), objectRotation);
 }
 
 void ABaseFood::SpawnObject(FVector Loc, FRotator Rot)
