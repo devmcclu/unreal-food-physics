5f0db3a77e1813638a40f8a8315309459c219b8f
Devlin McClure <mccluredt@gmail.com>
2020-02-29 13:25:19 -0500

    Create BaseFoodActor class

---
+++ Source/FoodPhysics/BaseFoodActor.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BaseFoodActor.h"
+#define print(text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::White,text)
+
+
+// Sets default values
+ABaseFoodActor::ABaseFoodActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionMesh = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	CollisionMesh->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ABaseFoodActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+    CollisionMesh->OnComponentBeginOverlap.AddDynamic(this, &ABaseFoodActor::OnOverlapBegin);
+	
+}
+
+// Called every frame
+void ABaseFoodActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseFoodActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	print(TEXT("Help me"));
+	FRotator objectRotation = FRotator();
+	SpawnObject(GetActorLocation(), objectRotation);
+}
+
+void ABaseFoodActor::SpawnObject(FVector Loc, FRotator Rot)
+{
+	FActorSpawnParameters SpawnParams;
+	AActor* SpawnedActorRef = GetWorld()->SpawnActor<AActor>(ActorToSpawn, Loc, Rot, SpawnParams);
+}
+
---
+++ Source/FoodPhysics/BaseFoodActor.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "FlexComponent.h"
+#include "GameFramework/Actor.h"
+#include "BaseFoodActor.generated.h"
+
+UCLASS()
+class FOODPHYSICS_API ABaseFoodActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseFoodActor();
+	
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UBoxComponent* CollisionMesh;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UFlexComponent* FlexMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Spawning")
+	TSubclassOf<AActor> ActorToSpawn;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void SpawnObject(FVector Loc, FRotator Rot);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
